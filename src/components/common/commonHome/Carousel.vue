ea283265b250ae38b4474644c0404465b6d1b3d3
kakapo <936555313@qq.com>
2021-01-06 18:09:44 +0800

    shijan

--- src/components/common/commonHome/Carousel.vue
+++ src/components/common/commonHome/Carousel.vue
@@ -1,30 +1,24 @@
 <template>
   <div>
     <div class="carousel-image">
-     <div class="main-carousel-image">
-      <ul
-        class="ul-image"
-        style="
-          transform: translate3d(0px, 0px, 0px);
-          transition-duration: 500ms;
-          "
-        ref="carouselImage"
-      >
+     <div class="main-carousel-image" ref="mainCarouselImage" @click="moveImage($event)" @mouseover="ChangeCursor">
+       <!-- <div class="pointer-arrow-left"></div>
+       <div class="pointer-arrow-right"></div> -->
+      <ul class="ul-image" style=" transform: translate3d(0px, 0px, 0px); transition-duration: 500ms;" ref="carouselImage">
         <li v-for="(item, index) in images" class="li-image" :key="index">
           <img :src="item.src" />
         </li>
-        <li v-if="images.length > 3" class="li-image">
-          <img :src="images[images.length - 1].src" />
-        </li>
       </ul>
      </div> 
-     <!-- <div class="carousel-pointer-image">
+
+     <div class="carousel-pointer-image">
        <ul class="ul-pointer-image">
-        <li v-for="(item, index) in images" :class="(imageIndex==index ? 'active-border ': ' ' )+'li-pointer-image'" :key="index" >
+        <li v-for="(item, index) in images" :class="(imagesIndex==index ? 'active-border ': ' ' )+'li-pointer-image'" :key="index" @mouseover="ShowImageByPointer(index)">
           <img :src="item.src" />
         </li>
       </ul>
-     </div> -->
+     </div>
+
     </div>
   </div>
 </template>
@@ -40,7 +34,7 @@ export default {
   data() {
     return {
       images: [
-        // { text: "图片一", src: require("assets/image/TestImage/1.gif") },
+        // { text: "图片7", src: require("assets/image/TestImage/8.gif") },
         { text: "图片2", src: require("assets/image/TestImage/2.jpg") },
         { text: "图片3", src: require("assets/image/TestImage/3.jpg") },
         { text: "图片4", src: require("assets/image/TestImage/4.jpg") },
@@ -50,22 +44,37 @@ export default {
       imagesIndex: 0,
     };
   },
+  watch:{
+    // imagesIndex:function(){
+    //   // console.log(this.imagesIndex);
+    // }
+  },
   created() {
     // this.imagesIndex=this.images.length
   },
   mounted() {
-    this.$nextTick(this.CarouselTimer());
+    // this.$nextTick(this.CarouselTimer());
   },
   methods: {
+    moveImage(e){
+      console.log(this.$refs.carouselImage.offsetLeft)
+       console.log(this.$refs.carouselImage.getBoundingClientRect().left+window.scrollX)
+      console.log(e)
+    },
+    ChangeCursor(){
+
+    },
+    ShowImageByPointer(index){
+      this.imagesIndex = index
+      this.$refs.carouselImage.style ="transform: translate3d(-" +620 * this.imagesIndex +"px, 0px, 0px); transition-duration: 0ms;";
+    },
     CarouselTimer() {
       setTimeout(() => {
-        if(this.imagesIndex >= this.images.length)
         this.$refs.carouselImage.style ="transform: translate3d(-" +620 * this.imagesIndex +"px, 0px, 0px); transition-duration: 500ms;";
-        console.log(this.$refs.carouselImage);
         if (this.imagesIndex >= this.images.length - 1) this.imagesIndex = 0;
         else this.imagesIndex++;
         this.CarouselTimer();
-      }, 1000);
+      }, 4000);
     },
   },
 };
@@ -76,6 +85,14 @@ export default {
   position: relative;
   overflow: hidden;
   // top: 100px;
+}
+.pointer-arrow-left{
+  position: absolute;
+  // z-index: 2;
+  overflow: hidden;
+  height: 100%;
+  width: 50%;
+  cursor: url(https://s10.mogucdn.com/mlcdn/c45406/190325_1da2399gei4fe7776hlaid8de796c.cur),copy!important;
 }
 .ul-image {
   width: 100%;
@@ -97,25 +114,31 @@ export default {
   height: 670px;
   width: 502px;
 }
-.carousel-pointer-image {
+.ul-pointer-image {
   width:620px;
   margin-top: 10px;
   display: flex;
+
 }
 .li-pointer-image{
   width: 50px;
-  height: 60px;
+  height: 50px;
   overflow: hidden;
   border-radius: 3px;
   margin-right: 6px;
   list-style: none;
+  margin-top: 2px;
+  // box-sizing:border-box;
+  // border:solid 2px #fff;
 
 }
 .ul-pointer-image img{
   width: 50px;
   height: 60px;
+    // border-radius: 3px;
 }
 .active-border{
+  margin-top: 0px;
   border:solid 2px red;
 }
 </style>
