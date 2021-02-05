f321465b58d85bd842c34725d5d8c7214f19fa43
kakapo <936555313@qq.com>
2021-02-05 18:49:14 +0800

    18点49分2021年2月5日

--- src/views/backstage/backstageitem/CategorySetting.vue
+++ src/views/backstage/backstageitem/CategorySetting.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+<el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+  <!-- <el-form :model="categoryForm">
+    <el-form-item label="活动名称" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="活动区域" :label-width="formLabelWidth">
+      <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-option label="区域一" value="shanghai"></el-option>
+        <el-option label="区域二" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+  </el-form> -->
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
     <div>
       <el-table
     :data="Categorys"
@@ -23,7 +40,7 @@
       label="缩略图"
       width="200">
       <template slot-scope="scope">
-            <div>{{scope.row.name}}</div>
+      <div>{{scope.row.name}}</div>
       </template>
     </el-table-column>
     <el-table-column
@@ -32,12 +49,9 @@
     </el-table-column>   
          <el-table-column label="操作">
       <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
+        <el-button type="text"
+          @click="handleEdit( scope.row)">编辑</el-button>
+        <el-button type="text"
           @click="handleDelete(scope.$index, scope.row)">删除</el-button>
       </template>
     </el-table-column>
@@ -52,21 +66,34 @@ export default {
 name:'BackStageSetting',
 data(){
   return{
-        Categorys:[]
+    dialogFormVisible:false,
+    Categorys:[],
+    categoryForm:{
+    id:'',
+    name:'',
+    description:'',
+    image:'',
+    parentId:'',
+    },
   }
 },
 beforeCreate(){
   GetAllCategory().then(
     response=>{ 
-     console.log(response);
+    //  console.log(response);
       this.Categorys = response.result;
-       console.log(this.Categorys);
+
+      //  console.log(this.Categorys);
     
     }
   )
 },
 methods:{
-
+  handleEdit(data){
+    this.categoryForm=data;
+    this.dialogFormVisible=true
+    console.log(data);
+  }
 }
 }
 </script>
